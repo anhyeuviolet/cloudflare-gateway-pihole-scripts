eb14f9e47a58dcf2d48725c1e8d02fc7bae5648d
Kenny Nguyen <anhyeuviolet@users.noreply.github.com>
2023-08-17 11:39:51 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -14,6 +14,7 @@ urls=(
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
   https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+  https://raw.githubusercontent.com/nextdns/click-tracking-domains/main/domains
 )
 
 # loop through the urls and download each file with curl
